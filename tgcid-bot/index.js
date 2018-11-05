9f1ca2a7e62d7e567eae605b69a46f14bdc58b18
marko delgadillo <markoneseven@gmail.com>
2018-11-04 18:55:15 -0800

    #3 Created the initial if/switch statement to detect various commands and keywords. Still not complete as it still needs to get the actual string with the exclamation point as well as iterate through an entire message for a specific word.

--- tgcid-bot/index.js
+++ tgcid-bot/index.js
@@ -2,6 +2,7 @@
 
 // Logging in to Discord
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -10,10 +11,26 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	console.log(message.content);
-	if (message.content === '!destroy') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Whom shall I destroy, Master?');
+	if (message.content.includes(`${prefix}`)) {
+		// find a new method of extracting the rest of the word from the word with the bang
+		const command = message.substr(1);
+		switch (command) {
+		case 'destroy':
+			message.channel.send('Whom shall I destroy, Master?');
+			break;
+		case 'help':
+			message.channel.send('How may I help you?');
+			break;
+		}
+	}
+	else {
+		message.content.forEach(word => {
+			switch (word) {
+			case '':
+				break;
+			}
+		});
 	}
 });
 
-client.login('NTA4MjAyMTIyODU3MjgzNTk2.DsEgrw.aW4kHYz48ALTzukRreUDatg3JA0');
+client.login(token);
