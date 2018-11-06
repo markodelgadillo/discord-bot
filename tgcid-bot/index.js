664ac9ffa19fdc237a68a612fdeb1e481c8a84b2
marko delgadillo <markoneseven@gmail.com>
2018-11-05 21:49:26 -0800

    #3 Added logic that will use regex to identify a command by the exclamation point at the bieginning of a word within a longer string and extract the command to use the string in a switch case.

--- tgcid-bot/index.js
+++ tgcid-bot/index.js
@@ -2,7 +2,7 @@
 
 // Logging in to Discord
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, keyword } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -12,8 +12,7 @@ client.once('ready', () => {
 client.on('message', message => {
 	console.log(message.content);
 	if (message.content.includes(`${prefix}`)) {
-		// find a new method of extracting the rest of the word from the word with the bang
-		const command = message.substr(1);
+		const command = message.content.match(/([!])\w+/)[0].substr(1);
 		switch (command) {
 		case 'destroy':
 			message.channel.send('Whom shall I destroy, Master?');
@@ -22,14 +21,6 @@ client.on('message', message => {
 			message.channel.send('How may I help you?');
 			break;
 		}
-	}
-	else {
-		message.content.forEach(word => {
-			switch (word) {
-			case '':
-				break;
-			}
-		});
 	}
 });
 
