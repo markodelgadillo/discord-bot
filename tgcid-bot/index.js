bd064e6008922dc1b316a4aa0d981b8035122afb
marko delgadillo <markoneseven@gmail.com>
2018-11-06 21:10:57 -0800

    #5 added a switch case for vulgar keywords that still needs refining.

--- tgcid-bot/index.js
+++ tgcid-bot/index.js
@@ -17,8 +17,34 @@ client.on('message', message => {
 		case 'destroy':
 			message.channel.send('Whom shall I destroy, Master?');
 			break;
-		case 'help':
-			message.channel.send('How may I help you?');
+		case 'butthelp':
+			message.channel.send(
+				'How may the Butt Stallion, the horse to end all horses help you?'
+			);
+			break;
+		}
+	}
+	// only detecting the first word of the array
+	else if (message.content.includes(...keyword.vulgar)) {
+		console.log('It is vulgar!');
+		const word = keyword.vulgar.filter(vulgarity =>
+			message.content.inlcudes(vulgarity)
+		);
+		console.log(word + ' here is the current word');
+		switch (word[0]) {
+		case 'fuck you':
+			message.channel.send(
+				'There will be no sexual intercourse here on MY watch!'
+			);
+			break;
+		case 'bitch':
+			message.channel.send('Hey now! Let us all calm down here!');
+			break;
+		case 'shit':
+			message.channel.send('You get that toilet talk outta here!');
+			break;
+		case 'ass':
+			message.channel.send('Someone talking about butts!?');
 			break;
 		}
 	}
